bed6f96c9251aeaa26281699ecc86afcf0f5ac8a
Mahmoud Martini <mahmoud5martini@gmail.com>
2021-09-08 18:13:55 +0300

    initial commit

---
+++ lib/model/todo.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/cupertino.dart';
+
+class TodoField {
+  static const cratedTime = 'createdTime';
+}
+
+class Todo {
+  DateTime createdTime;
+  String title;
+  String id;
+  String description;
+  bool isDone;
+
+  Todo({
+    @required this.createdTime,
+    @required this.title,
+    this.id,
+    this.description = '',
+    this.isDone = false,
+  });
+}
